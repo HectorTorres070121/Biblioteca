d70e14c98f6a6a1639a6589799895a41933049ad
eloy torres <20253rd037@utez.edu.mx>
2026-04-17 15:53:39 -0600

    tercer intento

---
+++ src/main/java/com/example/bibliotecamvc3/Controller/LoginController.java
@@ -0,0 +1,53 @@
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class LoginController {
+    @FXML
+    private UserModel userModel =new UserModel();
+    @FXML
+    protected TextField txtUsuario;
+    @FXML
+    protected TextField txtPassword;
+    @FXML
+    protected Label lbl;
+
+    @FXML
+    public void validarUsuario() {
+
+        String usuario = txtUsuario.getText();
+        String password = txtPassword.getText();
+
+        if (usuario.isEmpty() || password.isEmpty()) {
+            lbl.setText("Llena todos los campos");
+            return;
+        }
+
+        boolean valido = userModel.validarUsuario(usuario, password);
+
+        if (valido) {
+            lbl.setText("Bienvenido " + usuario);
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("segundaVista.fxml"));
+            try {
+                Parent root = loader.load();
+
+                Stage stage = new Stage();
+                stage.setTitle("segunda vista");
+                stage.setScene(new Scene(root));
+                stage.show();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+
+
+        } else {
+            lbl.setText("Datos incorrectos");
+        }
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.example.bibliotecamvc3 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.bibliotecamvc3 to javafx.fxml;
+    exports com.example.bibliotecamvc3;
+    exports com.example.bibliotecamvc3.Controller;
+    opens com.example.bibliotecamvc3.Controller to javafx.fxml;
+}
