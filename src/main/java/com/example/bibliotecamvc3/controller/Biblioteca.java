5bcb87d3be17d2ef6d462e62d07e89e5b11077ab
Evelinenkyang <20253rd059@utez.edu.mx>
2026-04-23 08:33:47 -0600

    se creo la logica de registrar libros

--- src/main/java/com/example/bibliotecamvc3/controller/Biblioteca.java
+++ src/main/java/com/example/bibliotecamvc3/controller/Biblioteca.java
@@ -5,7 +5,11 @@ import com.example.bibliotecamvc3.model.DatosBiblioteca;
 public class Biblioteca {
     DatosBiblioteca Datos = new DatosBiblioteca(0, "", "");
     //logica para mostrar y registrar libros
-
+    protected void onBotonRegistrarClick() {
+        try {
+            int id = Integer.parseInt(txtId.getText());
+            String titulo = txtTitulo.getText();
+            String autor = txtAutor.getText();
 
     //logica para registrar
 
